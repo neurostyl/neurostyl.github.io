d31ecb08289f286b6135317c94925f290b4d20b6
Alen Geo Mathew <66488088+alernkn247@users.noreply.github.com>
2024-09-23 22:04:31 +0100

    added react app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+    <footer className="footer_section">
+        <div className="container">
+          <p>
+            © <span id="displayDateYear" /> All Rights Reserved By
+            <a href="https://html.design/">Free Html Templates</a>
+          </p>
+        </div>
+      </footer>
+      </>
+  )
+}
+
+export default Footer
