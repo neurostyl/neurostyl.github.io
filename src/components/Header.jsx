ebd5304cf22ca347aaa8a540e4d2566257b29134
Excellent_Torch <ishanmaduranga42@gmail.com>
2024-10-14 17:48:12 +0100

    BG Video + Few Adjustments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,26 +3,16 @@ import React from 'react'
 function Header(props) {
   return (
     <>
-            <header className="header_section">
-          {/* <div className="header_top">
-            <div className="container-fluid">
-              <div className="contact_nav">
-                <a href="">
-                  <i className="fa fa-phone" aria-hidden="true" />
-                  <span>Call : {props.number}</span>
-                </a>
-                <a href="">
-                  <i className="fa fa-envelope" aria-hidden="true" />
-                  <span>Email : {props.email}</span>
-                </a>
-              </div>
-            </div>
-          </div> */}
+          <header >
+         
           <div className="header_bottom">
             <div className="container-fluid">
               <nav className="navbar navbar-expand-lg custom_nav-container ">
                 <a className="navbar-brand" href="index.html">
-                  <span>Neurostyl</span>
+             
+                     <img src = "../../assets/images/logo.png" width="212" height="52" alt="My Happy SVG"/>
+               
+               
                 </a>
                 {/* <button
                   className="navbar-toggler"
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,85 +1,68 @@
 import React from "react";
 import MeetTeam from "../components/MeetTeam";
+import DemoPlayback from "../../assets/videos/DemoPlayback.mp4";
+
 
 function Home() {
   return (
     <>
-      <div className="hero_area">
-        {/* slider section */}
-        <section className="slider_section ">
-          <div className="container ">
-            <div className="row">
-              {/* <div className="col-md-6 ">
-                <div className="detail-box">
-                  <h1>
-                    Repair and <br />
-                    Maintenance <br />
-                    Services
-                  </h1>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
-                    harum voluptatem adipisci. Quos molestiae saepe dicta nobis
-                    pariatur, tempora iusto, ad possimus soluta hic praesentium
-                    mollitia consequatur beatae, aspernatur culpa.
-                  </p>
-                  <a href="">Contact Us</a>
+      
+                <div className="bg-video">
+                  <video
+                    src={DemoPlayback}
+                    autoPlay
+                    loop
+                    muted
+                    type="video/mp4"
+                  />
                 </div>
-              </div> */}
-              <div className="col">
-                <div className="img-box">
-                <iframe width="100%" height="315" src="https://www.youtube.com/embed/zCLsP2nrR_c?si=4nUBd0r-psVhaxzU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-        {/* end slider section */}
-      </div>
 
-      <section className="about_section layout_padding-bottom">
+      <section className="professional_section layout_padding-bottom container">
         <div className="container">
           <div className="row">
             <div className="col-lg-5 col-md-6">
               <div className="detail-box">
-                <h2>Ishaan</h2>
-                <p>
-                  There are many variations of passages of Lorem Ipsum
-                  available, but the majority have suffered alteration in some
-                  form, by injected humour, or randomisedThere are many
-                  variations of passages of Lorem Ipsum available, but the
-                  majority have suffered alteration in some form, by injected
-                  humour, or randomised
-                </p>
-                <a href="">Read More</a>
+                <h1>Real Time</h1>
+                <h2>AI Style Transfer</h2>
+                
+                
               </div>
             </div>
             <div className="col-lg-7 col-md-6">
               <div className="img-box">
-                <img src="assets/images/about-img.jpg" alt="" />
+                <img src="../../assets/images/TitleImage.png" alt="" />
               </div>
             </div>
           </div>
         </div>
       </section>
-      <MeetTeam/>
-
-      <section className="info_section ">
+      <section className="image_section layout_padding-bottom container h2">
         <div className="container">
-          <h4>Get In Touch</h4>
+        <h2>Image Comparison Type Shit!</h2>
+          <p className="text-center">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque eligendi molestias voluptates quod commodi! Architecto reiciendis sint a, optio, voluptatibus modi animi fugiat totam officiis eos, quia ipsum. Quas, sint?
+          </p>
+        </div>
+        
+     
+         
+
+    </section>
+      <section className="about_section layout_padding-bottom container">
+     
+          <MeetTeam/>
+     
+       </section>
+      
+
+      <section className="info_section container">
+       
+          <h2>Get In Touch</h2>
           <div className="row">
             <div className="col-lg-10 mx-auto">
               <div className="info_items">
                 <div className="row">
-                  <div className="col-md-4">
-                    <a href="">
-                      <div className="item ">
-                        <div className="img-box ">
-                          <i className="fa fa-map-marker" aria-hidden="true" />
-                        </div>
-                        <p>Lorem Ipsum is simply dummy text</p>
-                      </div>
-                    </a>
-                  </div>
+                  
                   <div className="col-md-4">
                     <a href="">
                       <div className="item ">
@@ -104,7 +87,7 @@ function Home() {
               </div>
             </div>
           </div>
-        </div>
+      
         <div className="social-box">
           <h4>Follow Us</h4>
           <div className="box">
